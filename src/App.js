e9da08aa249ad4b5595b6da8fe2a7b5c00ccca68
James <kamaujames3705@gmail.com>
2023-04-18 12:35:23 +0300

    Npm start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Component2 from './Component2.js';
+
+function App() {
+  return (
+    <div className="App">
+
+        <Component2 />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component2.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Component2(){
+    return (
+         <div className="Audi">
+      <iframe
+        width="660"
+        height="415"
+        src="https://www.youtube.com/embed/Fx6QQBphdAc"
+        allow="autoplay; encrypted-media"
+        title="time video"
+        allowFullScreen
+      ></iframe>
+    </div>
+
+    );
+}
+
+
+export default Component2;
